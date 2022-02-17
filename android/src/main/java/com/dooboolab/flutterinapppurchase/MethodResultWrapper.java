0958d04154244e69d6695477cc0957c89b42e077
hanjingjing <jingjing.han@justalk.com>
2022-02-17 16:46:49 +0800

    Temporarily avoid crash: java.lang.IllegalStateException Reply already submitted

--- android/src/main/java/com/dooboolab/flutterinapppurchase/MethodResultWrapper.java
+++ android/src/main/java/com/dooboolab/flutterinapppurchase/MethodResultWrapper.java
@@ -5,59 +5,73 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 // MethodChannel.Result wrapper that responds on the platform thread.
 public class MethodResultWrapper implements Result {
-  private Result safeResult;
-  private MethodChannel safeChannel;
-  private Handler handler;
+    private Result safeResult;
+    private MethodChannel safeChannel;
+    private Handler handler;
+    private final String TAG = "MethodResultWrapper";
 
-  MethodResultWrapper(Result result, MethodChannel channel) {
-    safeResult = result;
-    safeChannel = channel;
-    handler = new Handler(Looper.getMainLooper());
-  }
+    MethodResultWrapper(Result result, MethodChannel channel) {
+        safeResult = result;
+        safeChannel = channel;
+        handler = new Handler(Looper.getMainLooper());
+    }
 
-  @Override
-  public void success(final Object result) {
-    handler.post(
-      new Runnable() {
-        @Override
-        public void run() {
-          safeResult.success(result);
-        }
-      });
-  }
+    @Override
+    public void success(final Object result) {
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            safeResult.success(result);
+                        } catch (IllegalStateException exception) {
+                            Log.e(TAG, "safeSuccess exception, result:" + result);
+                        }
+                    }
+                });
+    }
 
-  @Override
-  public void error(final String errorCode, final String errorMessage, final Object errorDetails) {
-    handler.post(
-      new Runnable() {
-        @Override
-        public void run() {
-          safeResult.error(errorCode, errorMessage, errorDetails);
-        }
-      });
-  }
+    @Override
+    public void error(final String errorCode, final String errorMessage, final Object errorDetails) {
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            safeResult.error(errorCode, errorMessage, errorDetails);
+                        } catch (IllegalStateException exception) {
+                            Log.e(TAG, "safeError exception, errorCode:" + errorCode + ";errorMessage:" + errorMessage + ";errorDetails:" + errorDetails);
+                        }
+                    }
+                });
+    }
 
-  @Override
-  public void notImplemented() {
-    handler.post(
-      new Runnable() {
-        @Override
-        public void run() {
-          safeResult.notImplemented();
-        }
-      });
-  }
+    @Override
+    public void notImplemented() {
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            safeResult.notImplemented();
+                        } catch (IllegalStateException exception) {
+                            Log.e(TAG, "safeNotImplemented exception" );
+                        }
+                    }
+                });
+    }
 
-  public void invokeMethod(final String method, final Object arguments) {
-    handler.post(
-      new Runnable() {
-        @Override
-        public void run() {
-          safeChannel.invokeMethod(method, arguments, null);
-        }
-      });
-  }
+    public void invokeMethod(final String method, final Object arguments) {
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        safeChannel.invokeMethod(method, arguments, null);
+                    }
+                });
+    }
 }
--- example/android/app/src/main/java/com/dooboolab/iapexample/MainActivity.java
+++ example/android/app/src/main/java/com/dooboolab/iapexample/MainActivity.java
@@ -3,11 +3,12 @@ package com.dooboolab.iapexample;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
   }
 }
